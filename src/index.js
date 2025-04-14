db323930c9fa1b6c77ab987a79eeb422997ff792
Harishkumar-J <harishwells@gmail.com>
2025-04-14 12:37:42 +0100

    connecting redux to the react App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+//1.1 provider to provide the store to the top level component
+import { Provider } from 'react-redux';
+//1.3 using the store
+import store from './store';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
+//1.2 provider
+//1.4 store prop
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Provider store={store}><App /></Provider>);
---
+++ src/store/index.js
@@ -0,0 +1,27 @@
+//1.1 importing the createStore function from redux
+import { createStore } from "redux";
+
+//1.4 reducer function has exsisting state and action as parameter
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+    if (action.type === "decrement") {
+      return {
+        counter: state.counter - 1,
+      };
+    }
+    return state;
+  }
+};
+//1.2 declare the store
+//1.3 createStore points the reducer function as the parameter
+const store = createStore(counterReducer);
+
+
+
+//1.5 export store to other components
+export default store;
+
+//1.6 we have to provide this store to the top level component which is the App.js
