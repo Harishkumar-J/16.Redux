876b595ce6c9a5ebdaee141b2b8f6ff6278ecdd9
Harishkumar-J <harishwells@gmail.com>
2025-04-16 17:24:22 +0100

    Attaching payloads to  Actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,36 @@
-//1.1 importing the createStore function from redux
+// //1.1 importing the createStore function from redux
+// import { createStore } from "redux";
+
+// //1.4 reducer function has exsisting state and action as parameter
+// const counterReducer = (state = { counter: 0 }, action) => {
+//   if (action.type === "increment") {
+//     return {
+//       counter: state.counter + 1,
+//     };
+//   }
+//     if (action.type === "decrement") {
+//       return {
+//         counter: state.counter - 1,
+//       };
+//     }
+//     return state;
+// };
+// //1.2 declare the store
+// //1.3 createStore points the reducer function as the parameter
+// const store = createStore(counterReducer);
+
+
+
+// //1.5 export store to other components
+// export default store;
+
+// //1.6 we have to provide this store to the top level component which is the App.js
+
+//1--------------------------------------------------------------------------------------------
+
+
 import { createStore } from "redux";
 
-//1.4 reducer function has exsisting state and action as parameter
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return {
@@ -13,15 +42,16 @@ const counterReducer = (state = { counter: 0 }, action) => {
         counter: state.counter - 1,
       };
     }
+    // 2.1 action and value 
+    if(action.type === 'increase-by-value'){
+      return{
+        counter: state.counter + action.value
+      }
+    }
     return state;
 };
-//1.2 declare the store
-//1.3 createStore points the reducer function as the parameter
 const store = createStore(counterReducer);
 
 
 
-//1.5 export store to other components
 export default store;
-
-//1.6 we have to provide this store to the top level component which is the App.js
